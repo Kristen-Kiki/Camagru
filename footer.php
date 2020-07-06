bb472bb2ae88f0674db5c4bb232489f904be09d7
Kristen McGregor <50993086+Kristen-Kiki@users.noreply.github.com>
2020-07-06 12:00:41 +0200

    Add files via upload
    footer updated

--- footer.php
+++ footer.php
@@ -7,8 +7,9 @@
 
 </HEAD>
 <BODY>
-<footer>
-  <p>@kmcgrego</p>
+<footer width="100%">
+  @kmcgrego  <a href="../profile/delete_acc.php"><div><button>DELETE MY ACCOUNT - - It's Time For Me To Leave This Universe</button></div></a>
+
 </footer>
 </BODY>
 </HTML>
