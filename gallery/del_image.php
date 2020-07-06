1d44e12d19b5e00a96086b768eac6ac3c3c5a6bf
Kristen McGregor <50993086+Kristen-Kiki@users.noreply.github.com>
2020-07-06 11:44:48 +0200

    Add files via upload
    deleting image updated

--- gallery/del_image.php
+++ gallery/del_image.php
@@ -1,15 +1,20 @@
   
 <?php
     require '../config/database.php';
-    $reg = $_GET['reg_date'];
-    try{
+
+    $capture_date = $_GET['reg_date'];
+    
+    try
+    {
 
         session_start();
-        $usr = $_SESSION['signin'];
-        $stmt = "DELETE FROM Images WHERE reg_date='$reg' AND UserID='$usr'";
-        $db_connect->exec($stmt);
-        echo "Image has been deleted successfully";
-        header('refresh:3; url="profile.php"');
+
+        $UserID = $_SESSION['login'];
+        $delete_image = "DELETE FROM Images WHERE reg_date='$capture_date' AND UserID='$UserID'";
+        $db_connect->exec($delete_image);
+        
+        echo "Image has been deleted";
+        header('refresh:3; url="./gallery.php"');
     }
 
     catch(PDOException $e)
