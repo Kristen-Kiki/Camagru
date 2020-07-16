d326d5b9fd9da054370957b129a439ee95507374
Kristen-Kiki <kmcgrego@student.wethinkcode.co.za>
2020-07-16 12:39:43 +0200

    delete image fixed - gallery updated to fix delete image

--- gallery/del_image.php
+++ gallery/del_image.php
@@ -2,23 +2,26 @@
 <?php
     require '../config/database.php';
 
-    $pic = $_GET['ImageID'];
+    if (!isset($_SESSION['login']) || $_SESSION['login'] === "")
+    header('Location: ../index.php');
     
-    try
+    else
     {
+        try
+        {
+            $stmt = $db_connect->query('SELECT * FROM Images');
+            $ImageID = $_POST['hidden'];
 
-        session_start();
+            $delete_image = "DELETE FROM Images WHERE ImageiD='$ImageID' LIMIT 1";
+            $db_connect->exec($delete_image);
+            
+            echo "Image has been deleted";
+            header('refresh:1; url="./gallery.php"');
+        }
 
-        $UserID = $_SESSION['login'];
-        $delete_image = "DELETE FROM Images WHERE ImageiD='$pic' LIMIT 1";
-        $db_connect->exec($delete_image);
-        
-        echo "Image has been deleted";
-        header('refresh:3; url="./gallery.php"');
-    }
-
-    catch(PDOException $e)
-    {
-        echo $sql . "<br>" . $e->getMessage();
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
     }
 ?>
